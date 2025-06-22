827f7535927195e56b2a2a45ecfa2bbaee33bd17
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-06-23 02:59:56 +0300

    Initial commit of javascript class and week 1 day 1 files

---
+++ week-1/intro.js
@@ -0,0 +1,9 @@
+console.log(1+1);
+console.log("Hello! Welcome to javascript");
+
+//Variable declaration
+let name ="Mary Magdalene";
+console.log(name);
+let x = 10;
+let y = 5;
+console.log(x+y);
