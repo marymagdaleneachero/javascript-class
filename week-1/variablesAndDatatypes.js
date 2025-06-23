fa23b23e887c4f9dca3d67f4576cb7a252041576
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-06-23 22:52:45 +0300

    Added file for variables and datatypes

---
+++ week-1/variablesAndDatatypes.js
@@ -0,0 +1,7 @@
+let studentName ="Jane"; //this is a string
+let age = 20;  //number
+let isRegistered = true;  //boolean
+let favoriteColor = null;  //null
+let phoneNumber;  //undefined datatype
+
+console.log(studentName,age,isRegistered,favoriteColor,phoneNumber);
