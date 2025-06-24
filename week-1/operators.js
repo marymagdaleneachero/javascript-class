201c561428a68fbaea715624ffb968b044a6a825
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-06-25 00:18:56 +0300

    Created 2 new files for operator and functions

---
+++ week-1/operators.js
@@ -0,0 +1,41 @@
+// //Assignment operator
+// let firstName = "Magdalene";
+// // x = yield;
+// // x = x+y;
+// // x += y;
+// // total = total + 20;
+// // total += 20;
+// // x = x-y;
+// // a = a+b;
+// // a = a*b;
+
+// //Arithmetic operators
+// const chickPrice = 1650;
+// const numberOfChicks = 100;
+// totalPrice = chickPrice * numberOfChicks;
+// console.log(totalPrice);
+
+// //Comparison operators
+// Used for boolean(true or false)
+// >= greater or equal
+// <= less than or equal
+// == loose equals(compares two objects)
+// === strict equals
+
+// if farmerAge >=20 && <=30{
+//     console.log("Eligible for chick request");
+// }
+
+// //Logical operators
+// And &&
+// OR ||
+// not !
+let a =5;
+let b = "5";
+console.log(a==b);
+console.log(a===b);
+
+//Concatenation
+let farmerName = "Maggie";
+let chickType = "Broiler";
+console.log("Good morning" + " " + farmerName,"you requested for" + " " + chickType +" "+"chicks");
--- week-1/variablesAndDatatypes.js
+++ week-1/variablesAndDatatypes.js
@@ -1,7 +1,47 @@
 let studentName ="Jane"; //this is a string
 let age = 20;  //number
-let isRegistered = true;  //boolean
+  //boolean
 let favoriteColor = null;  //null
 let phoneNumber;  //undefined datatype
-
+let isRegistered = true;
 console.log("name",studentName,"age",age,"registered",isRegistered,"color",favoriteColor,"number",phoneNumber);
+
+//Prices
+const chickPrice = 1650; //Number datatype in Ugx
+
+//Approval status
+let isApproved = true;
+
+//Farmer information
+let farmerName="Hellen Ikiror"; //string
+let farmerAge = 20;
+let isRegistered = true;
+
+//chick request
+let typeOfChick = "Broiler"
+
+
+console.log(typeof(chickPrice))
+
+//Non primitive data types(also called data structures coz they store massive data)
+//Array/list 1
+let chickType = ["Broiler","Layer"]
+
+
+//Object 1
+ let farmer = {
+    name :"Magdalene",
+    age : 20,
+    isRegistered : true
+}
+console.log(farmer,chickType)
+
+//Array 2
+let farmerNames =['Magdalene','Michael','Hellen','Melchizedek']
+
+//Object 2
+let chickStock ={
+    type : "layer",
+    age : 23,
+    quantity : 1000
+}
